412bfd1ed92b23235c2e4bea964be8be1e89fded
cuong.le-hung <cuonglehung1611@gmail.com>
2022-06-07 22:43:56 +0700

    clean workspace

--- install.sh
+++ install.sh
@@ -4,9 +4,14 @@ sudo apt update && sudo apt install -y neovim
 echo "Create workspace folder"
 mkdir -p ~/.config/nvim/
 
+echo "Installing the Vim-plug Plugin Manager"
+cd ~/.config/nvim/
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 echo "Copy files to ~/.config/nvim"
 cd ~/vim-config/config/nvim
 cp init.vim ~/.config/nvim/
 cp ginit.vim ~/.config/nvim/
 cp coc-settings.json ~/.config/nvim/
+
 
