a93c843c7f3754ac89eefbbb83fde014c7b1322a
leecuongdee1611 <leecuong.dee1611@gmail.com>
2022-06-07 16:53:49 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -8,10 +8,16 @@ echo "Installing the Vim-plug Plugin Manager"
 cd ~/.config/nvim/
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo "Copy files to ~/.config/nvim"
+echo "Copying files to ~/.config/nvim"
 cd ~/vim-config/config/nvim
 cp init.vim ~/.config/nvim/
 cp ginit.vim ~/.config/nvim/
 cp coc-settings.json ~/.config/nvim/
 
+echo "Installing NodeJS"
+curl -sL https://deb.nodesource.com/setup_16.x -o /tmp/nodesource_setup.sh
+nano /tmp/nodesource_setup.sh
+sudo bash /tmp/nodesource_setup.sh
+sudo apt install nodejs
+node -v
 
